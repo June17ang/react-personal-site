88742295490509e6a0075fc6db047f8366833ab5
Ang Chun Yee <chun1991.ang@gmail.com>
2021-01-09 13:20:19 +0800

    Add components layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+import Homepage from "./components/Homepage";
+import Portfolio from "./components/Portfolio";
+import Contactme from "./components/Contactme";
+
 function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <div>
+      <Homepage />
+      <Portfolio />
+      <Contactme />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Contactme.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class Contactme extends Component {
+  render() {
+    return <div>Contactme</div>;
+  }
+}
