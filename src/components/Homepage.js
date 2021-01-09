4ca3f364805f9601ee2a98696bf2f6d4bc27f338
Ang Chun Yee <chun1991.ang@gmail.com>
2021-01-09 13:28:44 +0800

    Add homepage json content

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,7 +1,14 @@
 import React, { Component } from "react";
+import details from "../jsons/Homepage.json";
+import "../styles/Homepage.css";
 
 export default class Homepage extends Component {
   render() {
-    return <div>Homepage</div>;
+    return (
+      <div id="homepage-section">
+        <div>{details.name}</div>
+        <div>{details.position}</div>
+      </div>
+    );
   }
 }
